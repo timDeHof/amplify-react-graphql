2525b9fc1518a483891acbdd7824332c0a8e4a95
timDeHof <this.is.tim.dehof@gmail.com>
2023-07-06 16:57:12 -0400

    added graphql and storage

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
+test("renders learn react link", () => {
+  render(<App signOut={undefined} />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
