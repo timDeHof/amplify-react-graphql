b797b14a833bccb8fdd29554fc6c6431cf6ff0ee
timDeHof <this.is.tim.dehof@gmail.com>
2023-07-06 15:13:53 -0400

    chore(.gitignore): update .gitignore to ignore Amplify files and directories feat(.vscode/settings.json): add settings to exclude Amplify files and directories in VS Code feat(amplify/.config/project-config.json): add project configuration for Amplify feat(amplify/backend/auth/amplifyreactgraphql293b804a/cli-inputs.json): add CLI inputs for Amplify authentication feat(amplify/backend/backend-config.json): add backend configuration for Amplify feat(amplify/backend/tags.json): add tags for Amplify resources feat(amplify/backend/types/amplify-dependent-resources-ref.d.ts): add types for Amplify dependent resources feat(amplify/cli.json): add CLI configuration for Amplify feat(amplify/team-provider-info.json): add team provider info for Amplify chore(package.json): add dependencies for Amplify feat(src/App.js): update App component to use Amplify UI components and add sign

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyreactgraphql293b804a": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
